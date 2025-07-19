24599d94833c3b2b62bffa1357c966cd0baa1fe6
mirzaalidev <mirzaalidev@gmail.com>
2025-07-19 13:05:40 +0500

    Remove README file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/myportfolio/", // 👈 Important for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
